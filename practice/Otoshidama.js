4308fc4ec4d386137be606b0a8123393cacd260f
ochi <ocos54ce@gmail.com>
2021-05-19 00:08:21 +0900

    otoshidamaのforを二重に修正

--- practice/Otoshidama.js
+++ practice/Otoshidama.js
@@ -4,25 +4,15 @@ function Main(input){
 	const total = parseInt(input[1],10)
 	const ary = []
 	
-	count :
 	for(let x = 0; x <= sheets; x++){
-		const t10000 = x * 10000
-		if(t10000 === total) {
-			ary.push([x,0,0])
-			break count
-		}
 		for(let y = 0; y <= sheets - x; y++){
+			const t10000 = x * 10000
 			const t5000 = y * 5000
-			if(t10000 + t5000  === total) {
-				ary.push([x,y,0])
-				break count
-			}
-			for(let z = 0; z <= sheets - x - y; z++){
-				const t1000 = z * 1000
-				if(t10000 + t5000 + t1000  === total) {
-					ary.push([x,y,z])
-					break count
-				}
+			const z = sheets - x - y 
+			const t1000 = z * 1000
+			if(t10000 + t5000 + t1000  === total) {
+				ary.push([x,y,z])
+				break 
 			}
 		}
 	}
