d5ba1bb3f64baf49a84abcaf1d743389bf907ce4
ochi <ocos54ce@gmail.com>
2021-05-28 23:12:18 +0900

    1.13 ABC061-B

--- cpp/1.13/ABC061B-CountingRoads.cpp
+++ cpp/1.13/ABC061B-CountingRoads.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main(){
+	int N, M; 
+	cin >> N >> M;
 	
+	vector<int> city(N);
+
+	for(int i = 0; i < M ; i++){
+		int a, b;
+		cin >> a >> b;
+
+		city.at(a - 1) += 1;
+		city.at(b - 1) += 1;
+
+	}
+
+	for(int i = 0; i < N; i++){
+		cout << city.at(i) << endl;
+	}
 }
---
+++ cpp/1.13/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+	// vector<int> city(N);
+
+	// for(int i = 0; i < M ; i++){
+	// 	int a, b;
+	// 	cin >> a >> b;
+
+	// 	city.at(a) += 1;
+	// 	city.at(b) += 1;
+	// }
+
+	// for(int i = 0; i < N; i++){
+	// 	cout << city << endl;
+	// }
