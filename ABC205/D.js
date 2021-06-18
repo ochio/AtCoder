856ce07a6393f4b2c458618b7bf0a2cc2cb894f6
ochi <ocos54ce@gmail.com>
2021-06-18 22:53:03 +0900

    add BigInt

--- ABC205/D.js
+++ ABC205/D.js
@@ -1,23 +1,27 @@
 function Main(input){
 	input = input.split("\n");
-	const Q = Number(input[0].split(' ')[1])
+	const Q = BigInt(input[0].split(' ')[1])
 	const A = input[1].split(' ').map(v => Number(v))
 	const c = []
-	for(let i = 0; i < A.length; i++){
-		const count = A[i] - (i + 1)
+	for(let i = 0n ; i < A.length; i++){
+		const toB = BigInt(A[i])
+		const count = toB - (i + BigInt(1))
 		c.push(count)
 	}
 
-	for(let i = 2; i < (Q + 2); i++){
-		const k = Number(input[i])
+	for(let i = 2n; i < (Q + BigInt(2)); i++){
+		const k = BigInt(input[i])
 		let ans
 		if(c[c.length-1] < k){
-			ans =  A[A.length - 1] + (k - c[c.length-1])
+			const toB = BigInt(A[A.length -  1])
+			ans = toB + (k - c[c.length-1])
 			
 		}else if(k <= c[c.length-1]){
 			const index = bs(c,k)
-			ans = (A[index] - 1) - (c[index] - k)
+			const toN = Number(A[index])
+			ans = (BigInt(toN) - BigInt(1)) - (c[index] - k)
 		}
+		ans = ans.toString().replace('n', '')
 		console.log(ans);
 		
 	}
@@ -33,7 +37,7 @@ function bs(arr, val, first = 0, last = arr.length){
 			last = mid;
 		}
 	}
-	return last;
+	return BigInt(last);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
