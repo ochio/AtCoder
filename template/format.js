8e9385ced1478deaac7d87f93991bd3b240cf006
ochi <ocos54ce@gmail.com>
2021-07-04 16:56:21 +0900

    to read test file at format

--- template/format.js
+++ template/format.js
@@ -4,4 +4,5 @@ function Main(input) {
 	input = input.split('\n');
 }
 
-Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+// Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+Main(require('fs').readFileSync('./test.txt', 'utf8'));
