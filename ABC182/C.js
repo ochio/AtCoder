3200846325e6ecd1c0d5872cf15691f919d5d4b1
ochi <ocos54ce@gmail.com>
2021-07-07 21:54:57 +0900

    solve ABC182 C

--- ABC182/C.js
+++ ABC182/C.js
@@ -2,14 +2,33 @@
 
 function Main(input) {
 	input = input.trim();
-	const N = Number(input);
 
-	for (let bit = 0; bit < 1 << (input.length - 1); bit++) {
-		for (let i = 0; i < input.length - 1; i++) {
-			console.log(i);
+	let count = 18;
+	if (Number(input) % 3 === 0) {
+		console.log(0);
+		return;
+	}
+	for (let bit = 0; bit <= (1 << input.length) - 1; bit++) {
+		const row = [];
+		for (let i = 0; i <= input.length - 1; i++) {
+			if (bit & (1 << i)) {
+				row.push(i);
+			}
+		}
+
+		let result = Array.from(input);
+
+		result = result.filter((v, i) => {
+			return row.indexOf(i) >= 0;
+		});
+
+		result = BigInt(result.join(''));
+		if (result != 0 && result % 3n == 0 && count > input.length - row.length) {
+			count = input.length - row.length;
 		}
 	}
+	count = count === 18 ? -1 : count;
+	console.log(count);
 }
 
-// Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-Main(require('fs').readFileSync('./test.txt', 'utf8'));
+Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
