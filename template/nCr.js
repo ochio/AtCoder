1cbbb26c720b9d3831d999d6fb6db17041a4edea
ochi <ocos54ce@gmail.com>
2021-07-08 23:51:09 +0900

    template nCr

---
+++ template/nCr.js
@@ -0,0 +1,14 @@
+function nCr(n, r) {
+	let ans = 1;
+	for (let i = n; i > n - r; i--) {
+		ans *= i;
+	}
+	if (r == 0) {
+		ans = 1;
+		return ans;
+	}
+	for (let i = r; i > 0; i--) {
+		ans /= i;
+	}
+	return ans;
+}
