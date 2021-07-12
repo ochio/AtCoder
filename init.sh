6d07a937b0d855c4a6b0aecdaea495eac499914a
ochi <ocos54ce@gmail.com>
2021-07-12 22:06:52 +0900

    escape overwrite at init

--- init.sh
+++ init.sh
@@ -8,5 +8,11 @@ if [ ! -d ./$CONTEST ]; then
   mkdir $CONTEST
 fi
 
+if [ ! -e ./$CONTEST/$FNAME ]; then
+  cp template/format.js $CONTEST/$FNAME
+else
+  ESC=$(printf '\033')
+  echo "${ESC}[31mAlready $CONTEST/$FNAME exist${ESC}[m"
+fi
+
 touch $CONTEST/test.txt
-cp template/format.js $CONTEST/$FNAME
