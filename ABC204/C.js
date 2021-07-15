8586f51d54a2959fe7629b27a5107a70f34ac02c
ochi <ocos54ce@gmail.com>
2021-07-15 20:05:45 +0900

    solve ABC204 C

--- ABC204/C.js
+++ ABC204/C.js
@@ -1,28 +1,65 @@
-function Main(input){
-	input = input.split("\n")
-	const [N, M] = input[0].trim().split(' ').map(Number)
-	const p = new Array(N).fill().map((v,i) => {
-		const tmp = new Array(N).fill(false)
-		tmp[i] = true
-		return v = tmp
-	})
-	for(let i = 0; i < M; i++){
-		const [Ai, Bi] = input[i + 1].trim().split(' ').map(Number)
-		p[Ai - 1][Bi - 1] = true
+class Graph {
+	constructor() {
+		this.connectedList = {};
 	}
 
-	function rec(v){
-		if(!p[v]) return
-		console.log(p[v]);
-		p[v].forEach(value => rec(value))
+	addVertex(vertex) {
+		this.connectedList[vertex] = [];
 	}
-	
-	let ans = 0;
-	for(let i = 0; i < N; i++){
-		rec(i)
-		ans += p[i].filter(value => value === true).length
+
+	addEdge(v1, v2) {
+		this.connectedList[v1].push(v2);
 	}
-	console.log("ans",ans);
+
+	dfs(start) {
+		const stack = [start];
+
+		const result = [];
+
+		const visited = {};
+
+		let currentVertex;
+
+		visited[start] = true;
+
+		while (stack.length) {
+			currentVertex = stack.pop();
+
+			result.push(currentVertex);
+
+			this.connectedList[currentVertex].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					stack.push(neighbor);
+				}
+			});
+		}
+		return result;
+	}
 }
 
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+const graph = new Graph();
+
+function Main(input) {
+	input = input.split('\n');
+	const [N, M] = input[0].split(' ').map(Number);
+
+	Array(N)
+		.fill(0)
+		.forEach((v, i) => {
+			graph.addVertex(i + 1);
+		});
+
+	for (let i = 0; i < M; i++) {
+		const [A, B] = input[i + 1].split(' ').map(Number);
+		graph.addEdge(A, B);
+	}
+
+	let ans = 0;
+	for (let i = 0; i < N; i++) {
+		ans += graph.dfs(i + 1).length;
+	}
+	console.log(ans);
+}
+
+Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
