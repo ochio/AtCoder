6b2f9e393b71abe9249c6964c5dc958cc6bede15
ochi <ocos54ce@gmail.com>
2021-07-18 23:11:30 +0900

    solve ARC123 A

--- ARC123/A.js
+++ ARC123/A.js
@@ -4,25 +4,12 @@ function Main(input) {
 	input = input.split('\n');
 	const [A, B, C] = input[0].split(' ').map(Number);
 
-	const d1 = B - A;
-	const d2 = C - B;
-	let ans;
+	const X = 2 * B - A - C;
+	const k = Math.max(0, Math.ceil(-X / 2));
 
-	if (d1 === d2) {
-		ans = 0;
-	} else if (d1 < d2) {
-		const tmp = Math.ceil(Math.abs(d2 - d1) / 2);
-		if (Math.abs(tmp - d2) - Math.abs(tmp + d1) === 1) {
-			ans = tmp + 1;
-		} else {
-			ans = tmp;
-		}
-	} else {
-		ans = Math.abs(d1 - d2);
-	}
+	const ans = X + 3 * k;
 
 	console.log(ans);
 }
 
-// Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-Main(require('fs').readFileSync('./test.txt', 'utf8'));
+Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
