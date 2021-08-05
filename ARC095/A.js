ff68d0cba95a87898e614887317a41e2b8d634a7
ochi <ocos54ce@gmail.com>
2021-08-05 22:18:38 +0900

    solve ARC095

--- ARC095/A.js
+++ ARC095/A.js
@@ -4,7 +4,17 @@ function Main(input) {
 	input = input.split('\n');
 	const N = Number(input[0]);
 	const ary = input[1].split(' ').map(Number);
+
+	const c = ary.slice();
+	c.sort((a, b) => a - b);
+
+	const left = c[N / 2 - 1];
+	const right = c[N / 2];
+
+	for (let i = 0; i < N; i++) {
+		const ans = ary[i] <= left ? right : left;
+		console.log(ans);
+	}
 }
 
-// Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-Main(require('fs').readFileSync('./test.txt', 'utf8'));
+Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
