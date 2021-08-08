3dab14538bf328c5992cdaa9a907f77ef4db9d2d
ochi <ocos54ce@gmail.com>
2021-08-08 23:33:54 +0900

    change shell to create many files

--- init.sh
+++ init.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 
 read -p "Contest: " CONTEST
-## read -p "JavaScript? (y/N): " yn
 read -p "File name: "  FNAME
 
 if [ ! -d ./$CONTEST ]; then
-  mkdir $CONTEST
+	mkdir $CONTEST
 fi
 
-if [ ! -e ./$CONTEST/$FNAME ]; then
-  cp template/format.js $CONTEST/$FNAME
-else
-  ESC=$(printf '\033')
-  echo "${ESC}[31mAlready $CONTEST/$FNAME exist${ESC}[m"
-fi
+ary=($FNAME)
+
+for FILE in "${ary[@]}"; do
+	if [ ! -e ./$CONTEST/$FILE ]; then
+		cp template/format.js $CONTEST/$FILE
+		echo "${ESC}[32mAlready $CONTEST/$FILE exist${ESC}[m"
+	else
+		ESC=$(printf '\033')
+		echo "${ESC}[31mAlready $CONTEST/$FILE exist${ESC}[m"
+	fi
+done
 
 touch $CONTEST/test.txt
