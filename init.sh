36c26a563791d30810bd2e7ecc41b8bcea36a26c
ochi <ocos54ce@gmail.com>
2021-08-09 10:47:03 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -8,13 +8,13 @@ if [ ! -d ./$CONTEST ]; then
 fi
 
 ary=($FNAME)
+ESC=$(printf '\033')
 
 for FILE in "${ary[@]}"; do
 	if [ ! -e ./$CONTEST/$FILE ]; then
 		cp template/format.js $CONTEST/$FILE
 		echo "${ESC}[32mAlready $CONTEST/$FILE exist${ESC}[m"
 	else
-		ESC=$(printf '\033')
 		echo "${ESC}[31mAlready $CONTEST/$FILE exist${ESC}[m"
 	fi
 done
