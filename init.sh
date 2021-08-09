84c6dc5659e4e407db067d1fcdab43eea060881b
ochi <ocos54ce@gmail.com>
2021-08-09 10:49:42 +0900

    change sentence

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ ESC=$(printf '\033')
 for FILE in "${ary[@]}"; do
 	if [ ! -e ./$CONTEST/$FILE ]; then
 		cp template/format.js $CONTEST/$FILE
-		echo "${ESC}[32mAlready $CONTEST/$FILE exist${ESC}[m"
+		echo "${ESC}[32mCreate $CONTEST/$FILE ${ESC}[m"
 	else
 		echo "${ESC}[31mAlready $CONTEST/$FILE exist${ESC}[m"
 	fi
