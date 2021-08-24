8ce1868a01327c8afb81c4dba95b50600e9e5420
ochi <ocos54ce@gmail.com>
2021-08-24 21:11:17 +0900

    add binarySearch

--- template/binary.js
+++ template/binary.js
@@ -1,16 +1,44 @@
-function lower_bound(ary, val, first = -1, last = ary.length) {
-	for (let m; last - first > 1; ) {
-		if (ary[(m = first + (0 | ((last - first) / 2)))] < val) first = m;
-		else last = m;
+function lowerBound(arr, n) {
+	let first = 0,
+		last = arr.length - 1,
+		middle;
+	while (first <= last) {
+		middle = 0 | ((first + last) / 2);
+		if (arr[middle] < n) first = middle + 1;
+		else last = middle - 1;
 	}
-	return last;
+	return first;
 }
 
-function upper_bound(ary, val, first = -1, last = ary.length) {
-	while (last - first > 1) {
-		const mid = first - Math.floor((last - first) / 2);
-		if (ary[mid] <= val) first = mid;
-		else last = mid;
+function upperBound(arr, n) {
+	let first = 0,
+		last = arr.length - 1,
+		middle;
+	while (first <= last) {
+		middle = 0 | ((first + last) / 2);
+		if (arr[middle] <= n) first = middle + 1;
+		else last = middle - 1;
 	}
-	return last;
+	return first;
+}
+function binarySearch(list, num) {
+	var index = -1;
+	var head = 0;
+	var tail = list.length;
+
+	while (head <= tail) {
+		var center = Math.floor((head + tail) / 2);
+		var centerVal = list[center];
+
+		if (centerVal === num) {
+			index = center;
+			break;
+		}
+		if (centerVal < num) {
+			head = center + 1;
+		} else {
+			tail = center - 1;
+		}
+	}
+	return index;
 }
