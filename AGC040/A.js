f48f8c42f266ff536a31e05f9d150a83b9eba710
ochi <ocos54ce@gmail.com>
2021-09-01 23:09:02 +0900

    solve AGC040 A

--- AGC040/A.js
+++ AGC040/A.js
@@ -3,19 +3,18 @@
 function Main(input) {
 	const S = input.trim();
 
-	let l = 0;
-	let m = 0;
-	let ans = 0;
+	const ary = Array(S.length + 1).fill(0);
+
 	for (let i = 0; i < S.length; i++) {
-		if (S[i] === '>') {
-			ans += Math.max(m, l);
-			m = 0;
-			l++;
-		} else if (S[i] === '<') {
-			ans += Math.max(m, l);
-			l = 0;
-			m++;
-		}
+		if (S[i] === '<') ary[i + 1] = Math.max(ary[i + 1], ary[i] + 1);
+	}
+	for (let i = S.length - 1; i >= 0; i--) {
+		if (S[i] === '>') ary[i] = Math.max(ary[i], ary[i + 1] + 1);
+	}
+
+	let ans = 0;
+	for (let i = 0; i < ary.length; i++) {
+		ans += ary[i];
 	}
 	console.log(ans);
 }
