8ec8c27c455eb591df1b91f6aaf548555b21cd22
ochi <ocos54ce@gmail.com>
2021-11-11 17:05:12 +0900

    solve ABC099 C

---
+++ ABC099/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let lim9 = 1;
+	// while (lim9 <= N) {
+	// 	let i = lim9;
+	// 	for (let j = i; j <= N; j++) {
+	// 		ary[j - 1] = Math.min(ary[j - 1], ary[j - i] + 1);
+	// 	}
+	// 	lim9 *= 6;
+	// }
