fb5ba5d8590688fd3cd13775cf1e30fd23e29d4b
ochi <ocos54ce@gmail.com>
2023-02-28 22:34:01 +0900

    add format

--- template/format.py
+++ template/format.py
@@ -1,6 +1,12 @@
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
+# str型で受け取るとき
+s = input()
+# int型で受け取るとき
+s = int(input())
+# float型　(小数)で受け取るとき
 s = float(input())
+# 文字列として受け取るとき
+A, B, C = input().split()
+# 入力値を整数で受け取るとき
+A, B = map(int, input().split())
+# list型で取得
+l = list(map(int, input().split()))
